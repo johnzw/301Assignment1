feac8cfe882c637d6183ab99cfd638341d66be5d
johnzw <rzzw456@126.com>
2014-01-30 15:54:25 -0700

    add some gson class into it, and add a saveinfile method

--- src/cs/se/assignment1/MainActivity.java
+++ src/cs/se/assignment1/MainActivity.java
@@ -1,7 +1,19 @@
 package cs.se.assignment1;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+
+import com.google.gson.Gson;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
@@ -15,12 +27,13 @@ public class MainActivity extends Activity {
 	private EditText editText;
 	private Button addButton;
 	private ListView listCounter;
+	private ArrayList<Counter> counterList;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-	}
+	}                     
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -35,8 +48,34 @@ public class MainActivity extends Activity {
 	public void addCounter(View view){
 		
 		Intent intent = new Intent(this, CounterActivity.class);
+		//Get the name from EditText, without considering the blank one, without name checking 
 		editText = (EditText) findViewById(R.id.editName);
-
+		String name = editText.getText().toString();
+		
+		//use this name to create a counter
+		Counter counter = new Counter(name);
+		this.saveInFile(counter);
+		
+		//Add counter to exsiting counter list
+		counterList.add(counter);
+		
+		
+	}
+	
+	private void saveInFile(Counter counter) {
+		Gson gson = new Gson();
+		try {
+			FileOutputStream fos = openFileOutput(FILENAME,
+					Context.MODE_APPEND);
+			fos.write((gson.toJson(counter)+"\n").getBytes());
+			fos.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
