d55ae71aaccd0eedc497eb780bf61f2a680dc0ba
johnzw <rzzw456@126.com>
2014-01-31 06:06:58 -0700

    function rename implemented

--- gen/cs/se/assignment1/R.java
+++ gen/cs/se/assignment1/R.java
@@ -49,7 +49,9 @@ public final class R {
         public static final int button_rename=0x7f050005;
         public static final int button_reset=0x7f050006;
         public static final int button_summarize=0x7f050008;
+        public static final int cancel=0x7f05000b;
         public static final int hello_world=0x7f050004;
+        public static final int ok=0x7f05000a;
         public static final int title_activity_counter=0x7f050003;
     }
     public static final class style {
--- src/cs/se/assignment1/CounterActivity.java
+++ src/cs/se/assignment1/CounterActivity.java
@@ -1,29 +1,37 @@
 package cs.se.assignment1;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 import com.google.gson.Gson;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.support.v4.app.NavUtils;
 import android.annotation.TargetApi;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Build;
 
 public class CounterActivity extends Activity {
-		private static final String FILENAME = "tempcounterfile.sav";
+		private static final String FILENAME2 = "tempcounterfile.sav";
+		private static final String FILENAME1 = "countersfile.sav";
 		private Counter theCounter;
 		private TextView nameText;
 		private TextView countsText;
-
+		private ArrayList<Counter> counterList = null;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -92,9 +100,52 @@ public class CounterActivity extends Activity {
 	}
 	
 	public void rename(View view){
-		
+		// 1. Instantiate an AlertDialog.Builder with its constructor
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		final EditText input = new EditText(this);
+		builder.setView(input);
+		builder.setNeutralButton(R.string.ok, new DialogInterface.OnClickListener() {
+	           public void onClick(DialogInterface dialog, int id) {
+	        	   String value = input.getText().toString();
+		            counterList = new ArrayList<Counter>();
+		            loadFromFile();
+		            
+		            //check if changed name has existed before
+
+		    		for(int i=0; i<counterList.size(); i++){
+		    			if(counterList.get(i).getName().equals(value)){
+		    				alertName();
+		    				return;
+		    			}
+		    		}
+		    		
+		    		for(int i=0; i<counterList.size(); i++){
+		    			if(counterList.get(i).getName().equals(theCounter.getName())){
+		    				counterList.get(i).editName(value);
+		    				break;
+		    			}
+		    		}
+		    		saveInFile();
+		    		theCounter.editName(value);
+		    		nameText.setText(theCounter.getName());
+	           }
+	    });
+		builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+	           public void onClick(DialogInterface dialog, int id) {
+	               // User cancelled the dialog
+	           }
+	    });
+
+		AlertDialog alert = builder.create();	
+		alert.show();
 	}
 	
+	public void alertName(){
+		AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+		alertDialog.setTitle("Sorry");
+		alertDialog.setMessage("the name has already existed, please change a name.");
+		alertDialog.show();
+	}
 	public void remove(View view){
 		theCounter.delete();
 		saveInFile(theCounter);
@@ -110,7 +161,7 @@ public class CounterActivity extends Activity {
 	private void saveInFile(Counter counter) {
 		Gson gson = new Gson();
 		try {
-			FileOutputStream fos = openFileOutput(FILENAME,
+			FileOutputStream fos = openFileOutput(FILENAME2,
 					Context.MODE_PRIVATE);
 			fos.write((gson.toJson(counter)).getBytes());
 			fos.close();
@@ -123,4 +174,45 @@ public class CounterActivity extends Activity {
 		}
 	}
 	
+	private void loadFromFile(){
+		Gson gson = new Gson();
+		Counter counter;
+		
+		try{
+			FileInputStream fis = openFileInput(FILENAME1);
+			BufferedReader in = new BufferedReader(new InputStreamReader(fis));
+			//each line is a json object
+			String line = in.readLine();
+			while(line != null){
+				counter = gson.fromJson(line, Counter.class);
+				counterList.add(counter);
+				line = in.readLine();
+			}
+		}catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	//very similar to saveinFile, but this time it overwrite the file with the counterList
+	private void saveInFile() {
+		Gson gson = new Gson();
+		try {
+			FileOutputStream fos = openFileOutput(FILENAME1,
+					Context.MODE_PRIVATE);
+			for(int i=0; i<counterList.size(); i++){
+				fos.write((gson.toJson(counterList.get(i))+"\n").getBytes());
+			}
+			fos.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
