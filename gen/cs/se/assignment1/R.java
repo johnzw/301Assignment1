4704f0ea5403dea17f6030980de540d7f83e065b
johnzw <rzzw456@126.com>
2014-01-31 00:58:54 -0700

    Counter UI designed 	very roughly

--- gen/cs/se/assignment1/R.java
+++ gen/cs/se/assignment1/R.java
@@ -24,10 +24,13 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080003;
-        public static final int add=0x7f080002;
-        public static final int counterList=0x7f080000;
-        public static final int editName=0x7f080001;
+        public static final int action_settings=0x7f080006;
+        public static final int add=0x7f080005;
+        public static final int button_increment=0x7f080002;
+        public static final int counterList=0x7f080003;
+        public static final int editName=0x7f080004;
+        public static final int name_counter=0x7f080000;
+        public static final int text_counts=0x7f080001;
     }
     public static final class layout {
         public static final int activity_counter=0x7f030000;
@@ -41,6 +44,11 @@ public final class R {
         public static final int action_settings=0x7f050001;
         public static final int add=0x7f050002;
         public static final int app_name=0x7f050000;
+        public static final int button_increment=0x7f050009;
+        public static final int button_remove=0x7f050007;
+        public static final int button_rename=0x7f050005;
+        public static final int button_reset=0x7f050006;
+        public static final int button_summarize=0x7f050008;
         public static final int hello_world=0x7f050004;
         public static final int title_activity_counter=0x7f050003;
     }
